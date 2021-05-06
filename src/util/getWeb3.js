6fc259ad387285a4728393f370ae9f929d66f946
Keriteal <keriteal985@gmail.com>
2021-05-06 14:35:03 +0800

    fix web3 not enabled

--- src/util/getWeb3.js
+++ src/util/getWeb3.js
@@ -7,15 +7,19 @@ import Web3 from 'web3'
 * 4. Get user account from metamask
 * 5. Get user balance
 */
+ethereum.autoRefreshOnNetworkChange = false
 
 let getWeb3 = new Promise(function (resolve, reject) {
   // Check for injected web3 (mist/metamask)
-  var web3js = window.web3
+  var web3js = window.ethereum
   if (typeof web3js !== 'undefined') {
-    var web3 = new Web3(web3js.currentProvider)
+    var web3 = new Web3(web3js)
+    window.ethereum.request({ method: 'eth_requestAccounts' }).then(accounts => {
+      console.log(accounts)
+    })
     resolve({
       injectedWeb3: web3.isConnected(),
-      web3 () {
+      web3() {
         return web3
       }
     })
@@ -30,10 +34,11 @@ let getWeb3 = new Promise(function (resolve, reject) {
       result.web3().version.getNetwork((err, networkId) => {
         if (err) {
           // If we can't find a networkId keep result the same and reject the promise
+          console.log(err)
           reject(new Error('Unable to retrieve network ID'))
         } else {
           // Assign the networkId property to our result and resolve promise
-          result = Object.assign({}, result, {networkId})
+          result = Object.assign({}, result, { networkId })
           resolve(result)
         }
       })
