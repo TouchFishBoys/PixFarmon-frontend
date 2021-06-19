52f12a722896965b81d1419d5c980b8886ed26cb
Keriteal <keriteal985@gmail.com>
2021-06-19 18:45:52 +0800

    Use cdn

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,49 @@
+const path = require("path");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+
+const resolve = dir => path.join(__dirname, dir);
+
+const cdn = {
+  externals: {
+    vue: "Vue",
+    vuex: "Vuex",
+    "vue-router": "VueRouter"
+  },
+  js: [
+    "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js",
+    "https://cdn.jsdelivr.net/npm/vuex@3.6.2/dist/vuex.min.js",
+    "https://cdn.jsdelivr.net/npm/vue-router@3.5.1/dist/vue-router.min.js"
+  ]
+};
+
 module.exports = {
   transpileDependencies: ["vuetify"],
   productionSourceMap: false,
-  configureWebpack: {
-    devtool: process.env.NODE_ENV === "production" ? undefined : "source-map",
-    resolve: {
-      alias: {
-        "@assets": "@/assets",
-        "@comps": "@/components",
-        "@views": "@/views",
-        "@dapp": "@/util/pixfarmon-dapp"
-      }
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      config.externals = cdn.externals;
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          test: new RegExp(`\\.(html|js|css)`),
+          threshold: 10240, // 只有大小大于该值的资源会被处理 10240
+          minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+          deleteOriginalAssets: true // 删除原文件
+        })
+      );
     }
   },
   chainWebpack: config => {
+    config.resolve.alias
+      .set("@assets", resolve("src/assets"))
+      .set("@comps", resolve("src/components"))
+      .set("@views", resolve("src/views"))
+      .set("@util", resolve("src/util"))
+      .set("@dapp", resolve("src/util/pixfarmon-dapp"));
     if (process.env.NODE_ENV === "production") {
       config.plugins.delete("prefetch");
-      config.externals({
-        web3: "web3"
+      config.plugin("html").tap(args => {
+        args[0].cdn = cdn;
+        return args;
       });
     }
   }
